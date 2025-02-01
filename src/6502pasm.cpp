9e152c2fa6b0dadb5c2b53de37a69abb0c484cd8
CalSch <calschwick@calschwick.net>
2025-01-31 22:51:39 -0600

    started work on instruction parsing (theres so much work to do!)

--- src/6502pasm.cpp
+++ src/6502pasm.cpp
@@ -6,8 +6,13 @@
 
 
 std::regex labelPattern("([a-zA-Z0-9_]+):\\s*");
-std::regex instructionPattern("^\\s*([a-zA-Z]+)( ([^,]*)(,(\\w))?)?\\s*$");
+std::regex instructionPattern("^\\s*([a-zA-Z]+)( (([^,\\s]*)(,(\\w))?))?\\s*$");
 std::regex directivePattern("\\s*\\.(\\w+)(\\s+(.+))?");
+
+std::regex immediatePattern("#\\$[0-9a-fA-F]{2}"); // #$BB (BB=byte)
+std::regex zeropagePattern("\\$[0-9a-fA-F]{2}");   //  $LL (LL=low byte of addr)
+std::regex absolutePattern("\\$[0-9a-fA-F]{4}");   //  $HHLL (HHLL = full addr)
+// std::regex indirectPattern("\\(\\$[0-9a-fA-F]{4}\\)");
 
 int Assembler::assemble() {
     for (int lineIdx=0;lineIdx<input_lines.size();lineIdx++) {
@@ -29,6 +34,9 @@ int Assembler::assemble() {
         if (lineType == LINE_LABEL) {
             parseLabel(lineNoComment);
         }
+        if (lineType == LINE_INSTRUCTION) {
+            parseInstruction(lineNoComment);
+        }
     }
     return 0;
 }
@@ -49,7 +57,7 @@ LineType Assembler::getLineType(std::string line) {
     return LINE_UNKNOWN;
 }
 
-int Assembler::parseLabel(std::string line) {
+Label Assembler::parseLabel(std::string line) {
     std::smatch m;
     std::regex_match(line,m,labelPattern);
 
@@ -63,7 +71,93 @@ int Assembler::parseLabel(std::string line) {
     }
 
     std::string name = m[1];
-    labels.push_back((Label){name,-1});
+    return (Label){name,-1}; // location is unknown for now
+}
 
-    return 0;
+Address Assembler::parseAddress(std::string str) {
+    if (str[0] == '%') { // it's a macro!
+        std::string macroName = str.substr(1); // get all characters after %
+        // Find the macro
+        Macro macro;
+        bool found = false;
+        for (int i=0;i<macros.size();i++) {
+            if (macros[i].name==macroName) {
+                macro = macros[i];
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            //TODO: error
+        } else {
+            str = macro.text; // Replace text and continue
+        }
+    }
+    if (str[0] == '#' && str[1] == '$' && str.size() == 4) { // Immediate (#$BB)
+        return (Address){
+            ADDR_IMMEDIATE,
+            stoi(str.substr(2,2),0,16) // convert hex str to int
+        };
+    }
+    if (str[0] == '$') {
+        if (str.size()==3) { // zeropage or relative, no index ($LL or $BB)
+            return (Address){
+                ADDR_ZERO_PAGE, // just returns zeropage, but later it will be converted into relative if its a branch instruction
+                stoi(str.substr(1,2),0,16) // convert hex str to int
+            };
+        }
+        if (str.size()==5) { // Either absolute no index or zeropage with index
+            // Now find which one!
+            if (str[3]==',') { // it's zeropage indexed!
+                if (str[4]=='X') {
+                    return (Address){
+                        ADDR_ZERO_PAGE_X,
+                        stoi(str.substr(1,2),0,16)
+                    };
+                }
+                if (str[4]=='Y') {
+                    return (Address){
+                        ADDR_ZERO_PAGE_Y,
+                        stoi(str.substr(1,2),0,16)
+                    };
+                }
+                // If there's no X or Y, keep going to return an unknown address
+            }
+            if (IS_HEX_CHAR(str[3])) { // it's absolute not indexed!
+                return (Address){
+                    ADDR_ABSOLUTE,
+                    stoi(str.substr(1,4),0,16)
+                };
+            }
+        }
+        
+    }
+    return (Address){
+        ADDR_INVALID,
+        -1
+    };
+}
+
+Instruction Assembler::parseInstruction(std::string line) {
+    std::smatch m;
+    std::regex_match(line,m,instructionPattern);
+
+    if (DEBUG) {
+        printf("Parsing instruction: '%s'\n",line.c_str());
+        int i=0;
+        for (auto group : m) {
+            printf("\tgroup %d: '%s'\n",i,group.str().c_str());
+            i++;
+        }
+    }
+
+    std::string mnemonic = m[1];
+    Address addr;
+    if (m[3]!="") {
+        addr = parseAddress(m[3]);
+        if (DEBUG) printf("\t(%d,%04x)\n",addr.addrMode,addr.location);
+    }
+
+    Instruction inst = (Instruction){mnemonic,addr};
+    return inst;
 }
--- src/6502pasm.h
+++ src/6502pasm.h
@@ -3,14 +3,23 @@
 #include <vector>
 #include <regex>
 
-#define DEBUG 0
+#define DEBUG 1
+
+#define IS_HEX_CHAR(c) ((c>='0' && c<='9') || (c>='a' && c<='f') || (c>='A' && c<='F'))
 
 // patterns
 extern std::regex labelPattern;
 extern std::regex instructionPattern;
 extern std::regex directivePattern;
 
+extern std::regex immediatePattern;
+extern std::regex zeropagePattern;
+extern std::regex absolutePattern;
+extern std::regex indirectPattern;
+
 enum AddressMode {
+    ADDR_INVALID, // For syntax errors
+    ADDR_IMPLIED,
     ADDR_ACCUMULATOR,
     ADDR_IMMEDIATE,
     ADDR_ZERO_PAGE,
@@ -22,20 +31,35 @@ enum AddressMode {
     ADDR_INDIRECT,
     ADDR_INDIRECT_X,
     ADDR_INDIRECT_Y,
-    ADDR_IMPLIED
 };
 
 enum LineType {
-    LINE_UNKNOWN,
+    LINE_UNKNOWN, // For syntax errors
     LINE_EMPTY,
     LINE_INSTRUCTION,
     LINE_LABEL,
     LINE_DIRECTIVE,
 };
 
+struct Macro {
+    std::string name;
+    std::string text;
+};
+
 struct Label {
     std::string name;
     int location;
+};
+
+struct Address {
+    AddressMode addrMode;
+    int location;
+    std::string label;
+};
+
+struct Instruction {
+    std::string mnemonic;
+    Address addr;
 };
 
 class Assembler {
@@ -44,14 +68,20 @@ public:
     std::vector<char> output;
 
     std::vector<Label> labels;
+    std::vector<Macro> macros;
 
     Assembler() {}
 
     LineType getLineType(std::string line);
 
-    int parseLabel(std::string line);
+    // Turns a string into an `Address`
+    Address parseAddress(std::string addr);
+    // Create and add a `Label` struct from a line
+    // Note: implies that the `labelPattern` already matches the line (No error handling!)
+    Label parseLabel(std::string line);
+    Instruction parseInstruction(std::string line);
 
-    int assembleInstruction();
+    int assembleInstruction(Instruction inst);
 
     int assemble();
 };
